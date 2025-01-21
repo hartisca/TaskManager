f1e7eace9b40474cc8427cc6f7487e37f2b8963b
Héctor <hartisca91@gmail.com>
2025-01-22 00:14:46 +0100

    Login y register componentes creados

--- task-manager-frontend/src/pages/Login.jsx
+++ task-manager-frontend/src/pages/Login.jsx
@@ -1,20 +1,31 @@
+import { useState } from "react";
 import Card from "../components/Card";
+import Button from "../components/ui/Button";
 
 function Login() {
-  return ( 
-    <Card title={"Login"}>
-      <form>
-        <label>
-          Email:
-          <input type="email" placeholder="Enter your email" />
-        </label>
-        <label>
-          Password:
-          <input type="password" placeholder="Enter your password" />
-        </label>
-        <button type="submit">Login</button>
-      </form>
-    </ Card>
+  const [ registered, setRegistered ] = useState(false)
+  
+  return (
+    <>
+     { !registered ? (<Card title={"Login"}>
+        <form>               
+          <input type="email" placeholder="Email" />                  
+          <input type="password" placeholder="Password" />
+          <a onClick={() => setRegistered(true)}>Create an account</a>    
+          <Button type="submit" text={"Submit"} onClick={""}>Login</Button>
+        </form>
+      </ Card>) : (
+        <Card title={"Register"}>
+        <form>               
+          <input type="email" placeholder="Email" />                  
+          <input type="password" placeholder="Password" />
+          <a onClick={() => setRegistered(!registered)}>Log in</a>    
+          <Button type="submit" text={"Submit"} onClick={""}>Login</Button>
+        </form>
+      </ Card>)
+      }
+    </>
+    
   );
 }
 
